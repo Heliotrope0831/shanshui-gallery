df9a61deda88cc35699c615943b06a6d4b321f21
Heliotrope0831 <15692317301@163.com>
2026-04-23 13:45:04 +0800

    Switch to Netlify friendly config

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react-swc' // 修正点：加上 plugin-
 // vite.config.ts
 export default defineConfig({
   plugins: [react()],
-  base: '/shanshui-gallery/', // 必须和你的 GitHub 仓库名完全一致，前后都要有斜杠
+  base: '/', // Netlify 喜欢这种简单的
 })
