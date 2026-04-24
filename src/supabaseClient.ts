f70d8b3f6fc4e7ff0a27b578f4425fd4842fec12
Heliotrope0831 <15692317301@163.com>
2026-04-24 14:40:41 +0800

    Fix: Switch to Legacy Anon Key

--- src/supabaseClient.ts
+++ src/supabaseClient.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
 const supabaseUrl = 'https://giokdgrpwuqlpzttthcm.supabase.co'
-const supabaseKey = 'sb_publishable_q4xQA6Zpia5_97XV4MDWwQ_gbvoJwH-' 
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imdpb2tkZ3Jwd3VxbHB6dHR0aGNtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzY4MzgyMTIsImV4cCI6MjA5MjQxNDIxMn0.X51HGryN8HynJLjKNxjCakvGl4eChXUtgbQv7rM-KIA' 
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
